360801477c1ef7846acad5e9dd03108577a9b488
SyedHasanAbbas8D <novareaper974@gmail.com>
2025-05-20 11:22:13 +0500

    Fix: Pass required model prop to ThreeModel in Scene

--- components/Sneaker/Scene.tsx
+++ components/Sneaker/Scene.tsx
@@ -9,7 +9,7 @@ const Scene = () => {
         <OrbitControls />
         <ambientLight intensity={0.5} />
         <directionalLight position={[10, 10, 10]} intensity={1} />
-        <ThreeModel />
+        <ThreeModel model="sneaker1" />
       </Canvas>
     </div>
   )
